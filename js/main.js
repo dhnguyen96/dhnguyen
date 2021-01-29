283d7831005cfefbb3e5609d0db079ea7a089897
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-01-29 00:09:46 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,6 +30,16 @@
         }
     }
 
+    //Close Nav
+    $(function(){
+        var navMain = $(".navbar-collapse"); // avoid dependency on #id
+        // "a:not([data-toggle])" - to avoid issues caused
+        // when you have dropdown inside navbar
+        navMain.on("click", "a:not([data-toggle])", null, function () {
+            navMain.collapse('hide');
+        });
+    });
+
     navbarFixed();
 
 
