c0e0524132eda4eb0a5f96abe35ae8fda1c33709
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-02-05 02:16:42 -0500

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -19,8 +19,8 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./firebase-messaging-sw.js')
     .then(function(registration) {
       console.log('Registration successful, scope is:', registration.scope);
-    }).then(function(getToken){
-        console.log(getToken);
+    }).then(function(token){
+        console.log(token);
     }).catch(function(err) {
       console.log('Service worker registration failed, error:', err);
     });
