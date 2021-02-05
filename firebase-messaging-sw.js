1b890a577b95a54be96862e076d6eb33a5589803
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-02-05 17:09:20 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -15,16 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.onBackgroundMessage` handler.
-messaging.onMessage((payload) => {
-    console.log('Message received while app is focused. ', payload);
-    // ...
-});
-
-
 messaging.onBackgroundMessage((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     // Customize notification here
