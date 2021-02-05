0a93168dcb57e287baf40fc51bdda48324c8a400
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-02-05 13:04:48 -0500

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -26,6 +26,22 @@ if ('serviceWorker' in navigator) {
     });
   }
 
+  // Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken({ vapidKey: '<YOUR_PUBLIC_VAPID_KEY_HERE>' }).then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
+
 messaging.onMessage(function(payload){
     console.log('Message received ', payload);
 })
