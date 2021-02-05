ab0cff8f54241154d82c36d5488da2a1c4c190e5
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-02-05 17:56:06 -0500

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -23,32 +23,18 @@ if('serviceWorker' in navigator) {
   .then(function(registration) {
    console.log("Service Worker Registered");
   messaging.useServiceWorker(registration);  
-    }); 
+    })
 }
 
 Notification.requestPermission().then(function(permission) {
     console.log('Permission granted.');
+    return messaging.getToken({vapidKey: "BA6plneO_Acs9ip6bWt1s1SOO3LLgZFFRXRxKruZ0KJ-bMpuY7y8CUukx1UGBLQGWB8N5cXrwjV2Yz-v-ICJIVs"});
 }).then(function (token) {
     console.log(token);
 }).catch(function (err) {
     console.log('Permission not granted.');
 });
 
-// Get registration token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-messaging.getToken({ vapidKey: 'BA6plneO_Acs9ip6bWt1s1SOO3LLgZFFRXRxKruZ0KJ-bMpuY7y8CUukx1UGBLQGWB8N5cXrwjV2Yz-v-ICJIVs' }).then((currentToken) => {
-    if (currentToken) {
-      // Send the token to your server and update the UI if necessary
-      // ...
-    } else {
-      // Show permission request UI
-      console.log('No registration token available. Request permission to generate one.');
-      // ...
-    }
-  }).catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
-    // ...
-  });
 messaging.onMessage(function(payload) {
     console.log('onMessage ', payload);
 });
