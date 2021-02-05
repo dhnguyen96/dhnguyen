d7c5b01e8c5f910d64888aa580965efca0f5610a
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-02-05 17:04:51 -0500

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -30,15 +30,39 @@ Notification.requestPermission().then(function(permission) {
     console.log('Permission granted.');
     return messaging.getToken({vapidKey: "BA6plneO_Acs9ip6bWt1s1SOO3LLgZFFRXRxKruZ0KJ-bMpuY7y8CUukx1UGBLQGWB8N5cXrwjV2Yz-v-ICJIVs"});
 }).then(function (token) {
+    saveToken(token);
     console.log(token);
 }).catch(function (err) {
     console.log('Permission not granted.');
 });
 
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken({ vapidKey: 'BA6plneO_Acs9ip6bWt1s1SOO3LLgZFFRXRxKruZ0KJ-bMpuY7y8CUukx1UGBLQGWB8N5cXrwjV2Yz-v-ICJIVs' }).then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
+
 messaging.onMessage(function(payload) {
     console.log('onMessage ', payload);
 });
 
+var tokenRefs = firebase.database().ref('token');
+
+function saveToken(token) {
+    newTokenRefs.set({
+        token: token
+    });
+}
 
 /******** Feedback submission *********/
 
