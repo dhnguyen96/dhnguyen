7e1444da3c999faa7fc9dc3ba6997c94903a17d5
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-02-05 13:29:29 -0500

    move

---
+++ dhnguyen/firebase-messaging-sw.js
@@ -0,0 +1,35 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.6/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC6gwKgxn_F7jMrafd_MuGOTmRhMxvIn2Q",
+    authDomain: "feedback-d5711.firebaseapp.com",
+    projectId: "feedback-d5711",
+    storageBucket: "feedback-d5711.appspot.com",
+    messagingSenderId: "322472703685",
+    appId: "1:322472703685:web:053cc16eec5bc1d66de2ac",
+    measurementId: "G-JF9PQNLH5M"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/firebase-logo.png",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+});
