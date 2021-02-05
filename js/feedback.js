1db8c6ece3a5b55837c0c6c20d19d704db15694b
dhnguyen96 <77410184+dhnguyen96@users.noreply.github.com>
2021-02-04 21:55:38 -0500

    Add files via upload

--- js/feedback.js
+++ js/feedback.js
@@ -69,34 +69,11 @@ NotisElem = document.getElementById("notis");
 ErrElem = document.getElementById("err")
 
 const messaging = firebase.messaging();
-messaging
-    .requestPermission()
-    .then(function () {
-        MsgElem.innerHTML = "Notification permission granted."
-        console.log("Notification permission granted.");
+navigator.serviceWorker.register('firebase-message-sw.js')
+.then((registration) => {
+  messaging.useServiceWorker(registration);
+});
 
-        // get the token in the form of promise
-        return messaging.getToken()
-    })
-    .then(function (token) {
-        // print the token on the HTML page
-        TokenElem.innerHTML = "token is : " + token
-    })
-    .catch(function (err) {
-        ErrElem.innerHTML = ErrElem.innerHTML + "; " + err
-        console.log("Unable to get permission to notify.", err);
-    })
-    
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/dhnguyen/firebase-messaging-sw.js')
-        .then(function(reg) {
-          // registration worked
-          console.log('Registration succeeded. Scope is ' + reg.scope);
-        }).catch(function(error) {
-          // registration failed
-          console.log('Registration failed with ' + error);
-        });
-      }
     
 /******** Feedback navbar *********/
 
